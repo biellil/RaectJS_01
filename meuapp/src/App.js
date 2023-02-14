2a8be62c2c0afaf7885a11a5700967a19b07d2d2
biel.lil <78098857+biellil@users.noreply.github.com>
2023-02-14 16:53:20 -0300

    Update App.js

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,7 +1,10 @@
 import react from "react";
 const Bemvindo = (props) => {
   return(
-    <div> <h2>Bem vindo {props.nome}</h2></div>
+    <div> 
+      <h2>Bem vindo {props.nome}</h2>
+      <h3>tem {props.idade} de idade</h3>
+    </div>
    
   );
 }
@@ -10,8 +13,9 @@ function App() {
   return(
     <div>
       Olá MUNDO!
-      <Bemvindo nome="biel"/>
-      <h1>react</h1>
+      <Bemvindo nome="biel" idade="21"/>
+      <Bemvindo nome="lana" idade="19"/>
+     
     </div>
   )
   
